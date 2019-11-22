75f05ff291a476b2bb45958145cfd03f68594916
bigdongdong <qipq@qq.com>
2019-11-23 01:37:52 +0800

    实现了adapter空布局，并且写法简单可以交互

--- app/src/main/java/com/chen/firstdemo/empty_recyclerview/EmptyAdapter.java
+++ app/src/main/java/com/chen/firstdemo/empty_recyclerview/EmptyAdapter.java
@@ -1,80 +1,121 @@
 package com.chen.firstdemo.empty_recyclerview;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class EmptyAdapter<EVH extends EViewHolder,
-        VH extends RecyclerView.ViewHolder>
-        extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+/*
+ * Create by chenxiaodong on 2019/11/23 0023 0:03
+ */
+abstract class EmptyAdapter<Bean,VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter{
+    protected final String TAG = "EmptyAdapter->TAG";
+    private boolean isInitialize = true ;  //初次加载，也就是recyclerView.setAdapter时，默认显示空白
+    private final int TYPE_EMPTY = -999999999 ;
+    private boolean isEmpty = false ;
 
-    private final String TAG = "EmptyAdapter->TAG";
-    protected boolean isOnlyEmptyView = false ;
+    protected Context context ;
 
+    List<Bean> listData ;
 
-    private final int TYPE_HEADER = -9999 ;
-    private final int TYPE_FOOTER = -9998 ;
-    private final int TYPE_EMPTY = -9997 ;
+    protected EmptyAdapter(Context context){
+        this.context = context ;
+    }
 
+    public void update(List<Bean> listData){
+        isInitialize = false ;
+        this.listData = listData ;
+        notifyDataSetChanged();
+    }
 
-    abstract EVH onCreateEmptyHolder(@NonNull ViewGroup viewGroup, int type);
-    abstract VH onCreateGeneralHolder(@NonNull ViewGroup viewGroup, int type);
+    public void append(List<Bean> listData){
+        isInitialize = false ;
+        if(this.listData == null){
+            this.listData = new ArrayList<>();
+        }
+        this.listData.addAll(listData);
+        notifyDataSetChanged();
+    }
 
-    abstract void onBindEViewHolder(EVH holder , int position);
-    abstract void onBindGeneralViewHolder(RecyclerView.ViewHolder holder , int position);
+    protected  List<Bean> getList(){
+        return this.listData;
+    }
+
+    protected  Bean getBean(int position){
+        if(listData!=null && position < listData.size()){
+            return listData.get(position);
+        }else {
+            return null ;
+        }
+    }
+
+    /**
+     * 之所以提供view的返回选项，是为了多元化空布局的操作空间，让空布局view可以跟用户交互
+     * @return 获取空布局的布局id或者view实例
+     */
+    protected abstract Object getEmptyIdOrView();
+
+    protected abstract VH onCreateViewHolder( int itemType ,ViewGroup viewGroup);
+
+    protected abstract void onBindViewHolder(VH holder, Bean bean , int position);
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int type) {
-        switch(type){
-            case TYPE_EMPTY:
-                //空布局
-                return onCreateEmptyHolder(viewGroup,type);
-            default:
-                return onCreateGeneralHolder(viewGroup,type);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if(i == TYPE_EMPTY){
+            if( getEmptyIdOrView() instanceof Integer){
+                return new EViewHolder(LayoutInflater.from(context).inflate((int) getEmptyIdOrView(),viewGroup,false));
+            }else if( getEmptyIdOrView() instanceof View){
+                return new EViewHolder((View) getEmptyIdOrView());
+            }else {
+                throw new RuntimeException(" getEmptyIdOrView()只接收布局id或者view类型！！！");
+            }
+        }else{
+            return onCreateViewHolder(i , viewGroup );
         }
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int i) {
-        if( holder instanceof EViewHolder){
-            onBindEViewHolder((EVH)holder ,i);
+        if(isEmpty == false){
+            onBindViewHolder((VH)holder,listData.get(i) , i);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if(isInitialize == true){
+            return 0 ;
+        }
+
+        if(listData == null || listData.size() == 0){
+            isEmpty = true;
+            return 1 ;
         }else{
-            onBindGeneralViewHolder(holder,i);
+            isEmpty = false ;
+            return listData.size();
         }
     }
 
     @Override
     public int getItemViewType(int position) {
-        Log.i(TAG, "getItemViewType: "+position);
-        if(isOnlyEmptyView && getItemCount() == 1 ){
+        if(getItemCount() == 1 && isEmpty == true){
             return TYPE_EMPTY ;
+        }else{
+            return super.getItemViewType(position);
         }
-        return super.getItemViewType(position);
-
     }
 
-    @Override
-    public int getItemCount() {
-        Log.i(TAG, "getItemCount: ");
-        if(isOnlyEmptyView){
-            return 1 ;
+    private class EViewHolder extends RecyclerView.ViewHolder{
+        public EViewHolder(@NonNull View itemView) {
+            super(itemView);
         }
-        return getGeneralItemCount();
     }
-
-    abstract int getGeneralItemCount();
-}
-
-abstract class EViewHolder extends RecyclerView.ViewHolder{
-    public EViewHolder(@NonNull View itemView) {
-        super(itemView);
-
-//        initView(itemView);
-    }
-//    abstract void initView(View itemView);
 }
 
--- app/src/main/java/com/chen/firstdemo/empty_recyclerview/EmptyAdapterActivity.java
+++ app/src/main/java/com/chen/firstdemo/empty_recyclerview/EmptyAdapterActivity.java
@@ -1,7 +1,6 @@
 package com.chen.firstdemo.empty_recyclerview;
 
 import android.content.Context;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,17 +12,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chen.firstdemo.R;
-import com.chen.firstdemo.utils.ScreenUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmptyAdapterActivity extends AppCompatActivity {
 
     Context context ;
     RecyclerView recyclerView ;
-    Button button ;
+    Button button1,button2,button3,button4 ;
+
+    List<String> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,81 +35,78 @@ public class EmptyAdapterActivity extends AppCompatActivity {
 
         context = this ;
         recyclerView = findViewById(R.id.recyclerView);
-        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
         recyclerView.setLayoutManager(new LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false));
 
-        MyAdapter adapter = new MyAdapter();
+        MyAdapter adapter = new MyAdapter(context) {
+            @Override
+            void deal(MyViewHolder holder, String data) {
+                holder.textView.setText(data);
+            }
+        };
         recyclerView.setAdapter(adapter);
 
-        button.setOnClickListener(view -> {
-            adapter.update(new String[]{"1","2","3","4"});
+        for (int i = 0; i < 1; i++) {
+            list.add("this is the "+i+" item");
+        }
+
+        button1.setOnClickListener(view -> {
+            adapter.update(list);
+        });
+        button2.setOnClickListener(view -> {
+            adapter.update(null);
+        });
+        button3.setOnClickListener(view -> {
+            adapter.update(new ArrayList<>());
+        });
+        button4.setOnClickListener(view -> {
+            adapter.append(list);
         });
     }
 
 
-    class MyAdapter extends EmptyAdapter<MyAdapter.EmptyViewHolder, MyAdapter.GeneralViewHolder>{
+    abstract class MyAdapter extends EmptyAdapter<String,MyAdapter.MyViewHolder> {
 
-        String[] words ;
-
-        public void update(String[] words){
-            this.words = words ;
-            if(this.words == null || this.words.length==0){
-                this.isOnlyEmptyView = true ;
-            }
-            notifyDataSetChanged();
-        }
-
-
-        @Override
-        EmptyViewHolder onCreateEmptyHolder(@NonNull ViewGroup viewGroup, int type) {
-            Log.i("aaa", "onCreateEmptyHolder: ");
-            ImageView emptyIV = new ImageView(context);
-            ViewGroup.LayoutParams params = new LinearLayout.LayoutParams(ScreenUtil.getScreenWidth(context),
-                    ScreenUtil.getScreenHeight(context));
-            emptyIV.setLayoutParams(params);
-            emptyIV.setImageResource(R.mipmap.empty);
-            return new EmptyViewHolder(emptyIV);
+        protected MyAdapter(Context context) {
+            super(context);
         }
 
         @Override
-        GeneralViewHolder onCreateGeneralHolder(@NonNull ViewGroup viewGroup, int type) {
-            return new GeneralViewHolder(LayoutInflater.from(context).inflate(R.layout.item_general,viewGroup,false));
+        protected Object getEmptyIdOrView() {
+            ImageView imageView = new ImageView(context);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(0,200,0,0);
+            imageView.setLayoutParams(params);
+            imageView.setImageResource(R.mipmap.empty);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context,"我被点击了！",Toast.LENGTH_SHORT).show();
+                }
+            });
+            return imageView;
         }
 
         @Override
-        void onBindEViewHolder(EmptyViewHolder holder, int position) {
-
+        protected MyViewHolder onCreateViewHolder(int itemType, ViewGroup viewGroup) {
+            return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_general,viewGroup,false));
         }
 
         @Override
-        void onBindGeneralViewHolder(RecyclerView.ViewHolder holder, int position) {
-            GeneralViewHolder viewHolder = (GeneralViewHolder)holder;
-            viewHolder.textView.setText(words[position]);
+        protected void onBindViewHolder(MyViewHolder holder, String s, int position) {
+            deal(holder,s);
         }
 
-        @Override
-        int getGeneralItemCount() {
-            if(words == null){
-                return 0;
-            }
-            return words.length;
-        }
+        abstract void deal(MyViewHolder holder , String data);
 
-        class EmptyViewHolder extends EViewHolder{
-            ImageView emptyIV ;
-            public EmptyViewHolder(@NonNull View itemView) {
-                super(itemView);
-
-                this.emptyIV = (ImageView) itemView;
-            }
-        }
-
-        class GeneralViewHolder extends RecyclerView.ViewHolder{
-
+        class MyViewHolder extends RecyclerView.ViewHolder{
             TextView textView ;
-            public GeneralViewHolder(@NonNull View itemView) {
+            public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
-
                 textView = itemView.findViewById(R.id.textView);
             }
         }
